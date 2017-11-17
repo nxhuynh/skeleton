1e2367b37215de09fd6a4204013aed541ea25592
Tom Huynh <nghia.huynh@utexas.edu>
2017-11-17 15:34:29 -0600

    [PART3] show list of accounts (addresses), default account and balance

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import {Rspan} from 'oo7-react';
-import {bonds} from 'oo7-parity';
+import {bonds, formatBalance} from 'oo7-parity';
 
 export class App extends React.Component {
     constructor() {
         super();
+        window.bonds = bonds;
     }
 
     render() {
         return (<div>
-            <Rspan>{bonds.height}</Rspan>
+            Accounts available:&nbsp;
+            <Rspan>{bonds.accounts.map(_=>_.join(', '))}</Rspan>
+            <br/>
+            Default account:&nbsp;
+            <Rspan>{bonds.me}</Rspan>
+            <br/>
+            With a balance of&nbsp;
+            <Rspan>{bonds.balance(bonds.me).map(formatBalance)}</Rspan>
         </div>);
     }
 }
