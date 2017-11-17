05c3e4493d27f0ab26916ddd24a9ee19c9fd2321
Tom Huynh <nghia.huynh@utexas.edu>
2017-11-17 15:22:59 -0600

    [PART3] print height of block chain

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import {Bond, TimeBond} from 'oo7';
 import {Rspan} from 'oo7-react';
-import {InputBond} from 'parity-reactive-ui';
-
-const computeColor = t => t.match(/^[0-9]+$/) ? {color: 'red'} : {color: 'black'}
-const format = ([msg, t]) => `${new Date(t)}: ${msg}`
+import {bonds} from 'oo7-parity';
 
 export class App extends React.Component {
     constructor() {
         super();
-        this.bond = new Bond();
-        this.time = new TimeBond();
     }
 
-	render() {
-		return (<div>
-		    <InputBond bond={this.bond} placeholder="Go ahead and type some text"/>
-            <Rspan style={this.bond.map(computeColor)}>
-                {Bond.all([this.bond, this.time]).map(format)}
-            </Rspan>
-		</div>);
-	}
+    render() {
+        return (<div>
+            <Rspan>{bonds.height}</Rspan>
+        </div>);
+    }
 }
