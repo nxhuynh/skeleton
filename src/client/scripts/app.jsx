299f2a54158a9d7f33be0b84ed1145035ff947c1
Tom Huynh <nghia.huynh@utexas.edu>
2017-11-16 18:13:37 -0600

    [PART3] Show timestamp of latest block in the blockchain

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Bond, TimeBond} from 'oo7';
 import {Rspan} from 'oo7-react';
 import {InputBond} from 'parity-reactive-ui';
+import {bonds} from 'oo7-parity';
 
 const computeColor = t => t.match(/^[0-9]+$/) ? {color: 'red'} : {color: 'black'}
 const format = ([msg, t]) => `${new Date(t)}: ${msg}`
@@ -9,16 +10,15 @@ const format = ([msg, t]) => `${new Date(t)}: ${msg}`
 export class App extends React.Component {
     constructor() {
         super();
-        this.bond = new Bond();
-        this.time = new TimeBond();
+        window.bonds = bonds;
     }
 
 	render() {
 		return (<div>
-		    <InputBond bond={this.bond} placeholder="Go ahead and type some text"/>
-            <Rspan style={this.bond.map(computeColor)}>
-                {Bond.all([this.bond, this.time]).map(format)}
-            </Rspan>
+            Latest block's timestamp is:&nbsp;
+            <Rspan style={{fontWeight: 'bold'}}>
+                {bonds.head.timestamp.map(_=>_.toString())}
+	        </Rspan>
 		</div>);
 	}
 }
