bb69469ea99150c0e9a1e3c83f40a9cad174e141
Tom Huynh <nghia.huynh@utexas.edu>
2017-11-25 13:04:38 -0600

    [PART4] interactive address and balance lookup given a name

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import {Bond} from 'oo7';
 import {Rspan} from 'oo7-react';
+import {InputBond} from 'parity-reactive-ui';
 import {bonds, formatBalance} from 'oo7-parity';
 
 export class App extends React.Component {
     constructor() {
         super();
         window.bonds = bonds;
+        this.bond = new Bond;
     }
 
    	render() {
-        var parity2 = bonds.registry.lookupAddress('parity-2', 'A');
-        bonds.namesOf(bonds.me).log();
         return(<div>
-            <Rspan>{bonds.registry.lookupAddress('parity-2', 'A')}</Rspan>
+            Address of <InputBond bond={this.bond} placeholder='Lookup a name' /> is:<br/>
+			<Rspan>{bonds.registry.lookupAddress(this.bond, 'A')}</Rspan>
             <br/>
-            <Rspan>{bonds.balance(parity2).map(formatBalance)}</Rspan>
+			Its balance is&nbsp;
+            <Rspan>
+			    {bonds.balance(bonds.registry.lookupAddress(this.bond, 'A')).map(formatBalance)}
+			</Rspan>
         </div>);
 	}
 }
