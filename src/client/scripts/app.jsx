ba00cb610474e914dd2ab4ad2e4a5b21c5a9d37d
Tom Huynh <nghia.huynh@utexas.edu>
2017-11-25 16:13:18 -0600

    [PART6] display vote count from a deployed contract

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -4,37 +4,67 @@ import {Rspan} from 'oo7-react';
 import {InputBond, HashBond, BButton, TransactionProgressLabel, TransactButton} from 'parity-reactive-ui';
 import {bonds, formatBalance, isNullData} from 'oo7-parity';
 
+const CounterABI = [
+          {
+            "constant": false,
+            "inputs": [{"name": "_option", "type": "uint256"}],
+            "name": "vote",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+          },
+          {
+            "constant": true,
+            "inputs": [{"name": "", "type": "address"}],
+            "name": "hasVoted",
+            "outputs": [{"name": "", "type": "bool"}],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+          },
+          {
+            "constant": true,
+            "inputs": [{"name": "", "type": "uint256"}],
+            "name": "votes",
+            "outputs": [{"name": "", "type": "uint256"}],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+          },
+          {
+            "anonymous": false,
+            "inputs": [
+                {"indexed": true, "name": "who", "type": "address"},
+                {"indexed": true, "name": "option", "type": "uint256"}
+            ],
+            "name": "Voted",
+            "type": "event"
+          }
+        ];
+
+    const Options = ['Red', 'Green', 'Blue'];
+
 export class App extends React.Component {
     constructor() {
         super();
-        window.bonds = bonds;
-        this.name = new Bond;
-        this.recipient = bonds.registry.lookupAddress(this.name, 'A');
-        this.state = { current: null };
-    }
-
-    give() {
-        this.setState({
-            current: bonds.post({
-                to: this.recipient,
-                value: 100 * 1e16
-            })
-        })
+        this.counter = bonds.makeContract('0x2f164799a258c1F65DD3B551ed16C53Dd844410c', CounterABI);
+        console.log('got here');
     }
 
    	render() {
         return(<div>
-            <InputBond bond={this.name} placeholder='Name of recipient' />
-            <TransactButton
-                content={this.name.map(n => `Give ${n} 1 ETH`)}
-                disabled={this.recipient.map(isNullData)}
-                tx={{
-                    to: this.recipient,
-                    value: 100 * 1e16
-                }}
-            />
-            <br/>
-            <TransactionProgressLabel value={this.state.current}/>
+            {Options.map((n, i) => (<div key={i}>
+                <Rspan style={{
+                    borderLeft: this.counter
+                        .votes(i)
+                        .map(v => `${1 + v * 10}px black solid`)
+                }}>
+                    <span style={{float: 'left', minWidth: '3em'}}>
+                        {n}
+                    </span>
+                </Rspan>
+            </div>))}
         </div>);
 	}
 }
