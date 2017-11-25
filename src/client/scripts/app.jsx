f550f40ce8ac2efcad6e1c1853fba0f52bcb6dee
Tom Huynh <nghia.huynh@utexas.edu>
2017-11-25 13:19:44 -0600

    [PART5] give 1 ETH to an account specified by name in the input box

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Bond} from 'oo7';
 import {Rspan} from 'oo7-react';
-import {InputBond} from 'parity-reactive-ui';
+import {InputBond, HashBond, BButton} from 'parity-reactive-ui';
 import {bonds, formatBalance} from 'oo7-parity';
 
 export class App extends React.Component {
@@ -9,6 +9,7 @@ export class App extends React.Component {
         super();
         window.bonds = bonds;
         this.bond = new Bond;
+        this.parity2 = bonds.registry.lookupAddress('parity-2', 'A');
     }
 
    	render() {
@@ -20,6 +21,11 @@ export class App extends React.Component {
             <Rspan>
 			    {bonds.balance(bonds.registry.lookupAddress(this.bond, 'A')).map(formatBalance)}
 			</Rspan>
+            <br/>
+            <BButton
+                content='Give 1'
+                onClick={() => bonds.post({to: bonds.registry.lookupAddress(this.bond, 'A'), value: 100*1e16})}
+            />
         </div>);
 	}
 }
