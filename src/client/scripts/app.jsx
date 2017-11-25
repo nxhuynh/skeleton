ef6e2ea500c7389a41d730ec05cf166c541eba60
Tom Huynh <nghia.huynh@utexas.edu>
2017-11-25 13:51:33 -0600

    [PART5] improve previous implementation

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -2,29 +2,23 @@ import React from 'react';
 import {Bond} from 'oo7';
 import {Rspan} from 'oo7-react';
 import {InputBond, HashBond, BButton} from 'parity-reactive-ui';
-import {bonds, formatBalance} from 'oo7-parity';
+import {bonds, formatBalance, isNullData} from 'oo7-parity';
 
 export class App extends React.Component {
     constructor() {
         super();
         window.bonds = bonds;
-        this.bond = new Bond;
-        this.parity2 = bonds.registry.lookupAddress('parity-2', 'A');
+        this.name = new Bond;
+        this.recipient = bonds.registry.lookupAddress(this.name, 'A');
     }
 
    	render() {
         return(<div>
-            Address of <InputBond bond={this.bond} placeholder='Lookup a name' /> is:<br/>
-			<Rspan>{bonds.registry.lookupAddress(this.bond, 'A')}</Rspan>
-            <br/>
-			Its balance is&nbsp;
-            <Rspan>
-			    {bonds.balance(bonds.registry.lookupAddress(this.bond, 'A')).map(formatBalance)}
-			</Rspan>
-            <br/>
+            <InputBond bond={this.name} placeholder='Name of recipient' />
             <BButton
-                content='Give 1'
-                onClick={() => bonds.post({to: bonds.registry.lookupAddress(this.bond, 'A'), value: 100*1e16})}
+                content={this.name.map(n => `Give ${n} 1 ETH`)}
+                disabled={this.recipient.map(isNullData)}
+                onClick={() => bonds.post({to: this.recipient, value: 100 * 1e16})}
             />
         </div>);
 	}
