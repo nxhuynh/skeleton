1373d047454673da0fabeb126bd59e909d8d90ab
Tom Huynh <nghia.huynh@utexas.edu>
2017-11-26 00:29:23 -0600

    [staging] example showing how to use value returned from contract

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -3,57 +3,234 @@ import {Bond} from 'oo7';
 import {Rspan} from 'oo7-react';
 import {InputBond, HashBond, BButton, TransactionProgressLabel, TransactButton} from 'parity-reactive-ui';
 import {bonds, formatBalance, isNullData} from 'oo7-parity';
+import {equals} from 'bignumber.js';
 
-const CounterABI = [
-          {
-            "constant": false,
-            "inputs": [{"name": "_option", "type": "uint256"}],
-            "name": "vote",
-            "outputs": [],
-            "payable": false,
-            "stateMutability": "nonpayable",
-            "type": "function"
-          },
-          {
-            "constant": true,
-            "inputs": [{"name": "", "type": "address"}],
-            "name": "hasVoted",
-            "outputs": [{"name": "", "type": "bool"}],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "constant": true,
-            "inputs": [{"name": "", "type": "uint256"}],
-            "name": "votes",
-            "outputs": [{"name": "", "type": "uint256"}],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "anonymous": false,
-            "inputs": [
-                {"indexed": true, "name": "who", "type": "address"},
-                {"indexed": true, "name": "option", "type": "uint256"}
-            ],
-            "name": "Voted",
-            "type": "event"
-          }
-        ];
+const LinkedListABI = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "length",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "total",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_number",
+        "type": "uint256"
+      },
+      {
+        "name": "_name",
+        "type": "bytes32"
+      }
+    ],
+    "name": "addEntry",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "setTotal",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "head",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_id",
+        "type": "bytes32"
+      }
+    ],
+    "name": "getEntry",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bytes32"
+      },
+      {
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "writtenTotal",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "name": "objects",
+    "outputs": [
+      {
+        "name": "next",
+        "type": "bytes32"
+      },
+      {
+        "name": "number",
+        "type": "uint256"
+      },
+      {
+        "name": "name",
+        "type": "bytes32"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "resetTotal",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "name": "head",
+        "type": "bytes32"
+      },
+      {
+        "indexed": false,
+        "name": "number",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "name",
+        "type": "bytes32"
+      },
+      {
+        "indexed": false,
+        "name": "next",
+        "type": "bytes32"
+      }
+    ],
+    "name": "AddEntry",
+    "type": "event"
+  }
+];
 
-    const Options = ['Red', 'Green', 'Blue'];
 
 export class App extends React.Component {
     constructor() {
         super();
-        this.counter = bonds.makeContract('0x2f164799a258c1F65DD3B551ed16C53Dd844410c', CounterABI);
-        this.state = { tx: null };
-        this.prevVote = this.counter.Voted({ who: bonds.me });
+        this.linkedlist = bonds.makeContract('0x4B906e65401AEc3722ce007b51d5B547fc336fD6', LinkedListABI);
+        //this.prevVote = this.counter.Voted({ who: bonds.me });
+        //this.linkedlist.total().log();
+        this.head = this.linkedlist.head();
+        this.node_is = new Bond;
+        this.cur_node = this.head;
+        console.log(typeof this.linkedlist.length().c);
+        console.log(this.linkedlist.length().c.constructor.name);
+        console.log(this.linkedlist.length().c[0] == 2);
+        var len = this.linkedlist.length();
+        len.then(b => console.log(b.equals('2')));
+        //len.log();
     }
 
    	render() {
+        //this.head.log();
+        console.log('====');
+        //this.linkedlist.getEntry(this.head).log();
+        console.log('*****');
+/*
+        var total = this.linkedlist.total().c[0];
+        console.log(total);
+        console.log(head.constructor.name);
+*/
+        /*
         return(<div>
             {Options.map((n, i) => (<div key={i}>
                 <Rspan style={{
@@ -76,6 +253,23 @@ export class App extends React.Component {
             <Rspan>
                 {this.prevVote.map(v => v.length > 0 ? `Already voted for ${Options[v[0].option]}` : '')}
             </Rspan>
+        </div>);
+        */
+        return(<div>
+            <a onClick={() => {
+                var cur_node = this.linkedlist.objects(this.cur_node);
+                cur_node.then(cur => {
+                    console.log(cur[0] != '');
+                    if (cur[0] != '')) {
+                        cur.log();
+                        //this.cur_node = cur_node[0];
+                    } else {
+                        console.log('reached the end of linked list');
+                    }
+                });
+            }>
+                getEntry
+            </a>
         </div>);
 	}
 }
